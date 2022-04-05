1f17dd2fde58981374afe7f043961716da78aac6
dkysel <100780866+dkysel@users.noreply.github.com>
2022-04-05 10:26:14 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ resource "null_resource" "resource3" {
   provisioner "local-exec" {
     command = "echo $ENV"
     environment = {
-      ENV = "Hello World!X🇺🇦X---"
+      ENV = "Hello World! 🇺🇦"
     }
  }
  
